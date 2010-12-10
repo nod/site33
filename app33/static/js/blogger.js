448010bf732ed90c2a4d1d590316424c329260ca
jeremy <jeremy@33ad.org>
2010-12-10 04:09:45 +0000

    fixed NaN junk with twitter api

--- app33/static/js/blogger.js
+++ app33/static/js/blogger.js
@@ -15,8 +15,9 @@ function formatTweets(tweets) {
 }
 
 function relative_time(time_value) {
-  var values = time_value.split(" ");
+  /*var values = time_value.split(" ");
   time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+*/
   var parsed_date = Date.parse(time_value);
   var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
   var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
